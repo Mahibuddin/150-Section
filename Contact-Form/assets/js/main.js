d00e3297c727181f72f6935f4bceace2a7689152
mahib uddin <mahibuddin6@gmail.com>
2019-09-16 13:53:25 +0600

    contact-form

---
+++ Contact-Form/assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    // $('[data-toggle="datepicker"]').datepicker();
+    $("#showDate").datepicker({
+        //appendText:'mm/dd/yyyy',
+        //showOn:'both',
+        //buttonText:'Show Date',
+        dateFormat:'dd/mm/yy',
+        changeMonth:true,
+        changeYear:true
+    });
+
+
+
+
+})
+
