fd611945d1d1cca8ac1d891ce0fa3ec0f2a52ebf
mahib uddin <mahibuddin6@gmail.com>
2019-09-19 15:17:55 +0600

    OwlCarousel Section complete

--- section-team-carousel/assets/js/main.js
+++ section-team-carousel/assets/js/main.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
 
     $(".staff-list").owlCarousel({
         items: 4,
-        autopaly: false
+        autopaly: false,
+        margin: 30,
+        loop: true,
+        nav: true,
+        navText: ["<i class='fas fa-long-arrow-alt-left'></i>", "<i class='fas fa-long-arrow-alt-right'></i>"]
     });
 
 
