13cb04fc6dee3fe77edfd495e29f3090c758e5ef
mahib uddin <mahibuddin6@gmail.com>
2019-10-06 21:58:58 +0600

    Isotope Filtering

---
+++ isotop-filtering-29/assets/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    // $(".project-title li").on('click',function(){
+    //     var selector = $(this).attr('data-filter');
+    //     $(".project-list").isotope({
+    //         filter = selector
+    //     })
+    // });
+
+    $(".project-list").isotope({
+
+    });
+
+
+})
+
+
+
